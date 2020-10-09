ff6807b8139afc21745db85e77f06db9fa9e2304
Vriyas Hartama <vriyashartama@gmail.com>
2020-10-09 21:16:22 +0700

    databinding

--- app/src/main/java/com/tugasbesar/alamart/HomeFragment.java
+++ app/src/main/java/com/tugasbesar/alamart/HomeFragment.java
@@ -2,14 +2,42 @@ package com.tugasbesar.alamart;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.facebook.shimmer.ShimmerFrameLayout;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.tugasbesar.alamart.databinding.FragmentHomeBinding;
+import com.tugasbesar.alamart.item.Item;
+import com.tugasbesar.alamart.item.ItemAdapter;
+import com.tugasbesar.alamart.item.ItemSpaceDecorator;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class HomeFragment extends Fragment {
 
+    private FragmentHomeBinding fragmentHomeBinding;
+    private ItemAdapter adapter;
+    private SwipeRefreshLayout refreshLayout;
+    private RecyclerView recyclerView;
+    private CollectionReference firestore;
+    private ShimmerFrameLayout shimmerFrameLayout;
+
+    private List<Item> items = new ArrayList<Item>();
 
     public HomeFragment() {}
 
@@ -19,9 +47,53 @@ public class HomeFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        fragmentHomeBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false);
+        View view = fragmentHomeBinding.getRoot();
+
+        recyclerView = view.findViewById(R.id.recycler_view_item);
+        refreshLayout = view.findViewById(R.id.swipe_refresh);
+        shimmerFrameLayout = view.findViewById(R.id.shimmer_item_container);
+
+        firestore = FirebaseFirestore.getInstance().collection("items");
+
+
+        adapter = new ItemAdapter(getContext(), items);
+        fragmentHomeBinding.setAdapter(adapter);
+        recyclerView.addItemDecoration(new ItemSpaceDecorator(30));
+
+        refreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                getItem();
+                refreshLayout.setRefreshing(false);
+            }
+        });
+
+        getItem();
+
+        return view;
+    }
+
+    private void getItem() {
+        shimmerFrameLayout.startShimmer();
+        shimmerFrameLayout.setVisibility(View.VISIBLE);
+        recyclerView.setVisibility(View.GONE);
+        firestore.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    items.clear();
+                    for (QueryDocumentSnapshot qdc : Objects.requireNonNull(task.getResult())) {
+                        items.add(qdc.toObject(Item.class));
+                    }
+                    adapter.notifyDataSetChanged();
+                    shimmerFrameLayout.stopShimmer();
+                    shimmerFrameLayout.setVisibility(View.GONE);
+                    recyclerView.setVisibility(View.VISIBLE);
+                }
+            }
+        });
     }
 }
